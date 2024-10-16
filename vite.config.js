b27a05ebf9046afb824b55d14962b340b0fa8c23
Ilia Markovski <i.markovski@jdm.de>
2024-10-16 16:18:48 +0300

    Update vite config to build all pages

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        deviceFirebase: './pocs/device-firebase/index.html',
+        webFirebase: './pocs/web-firebase/index.html',
+        webVanilla: './pocs/web-vanilla/index.html',
+      }
+    }
+  }
 })
